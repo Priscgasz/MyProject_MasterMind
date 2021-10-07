45e86f0fa29cc6897b9cd126c87b358d399a227d
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-10-07 18:22:01 +0200

    add css and audio

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ let guessNumber = 1;
 let clicks = 1; 
 let turn = 1;
 
-const colors = ['green', 'purple', 'red', 'yellow','blue','black'];
+const colors = ['green', 'purple', 'red', 'yellow','blue','grey'];
+
+let audiotest = new Audio("/Johnny Jewel - The Hacker [Music Inspired By Ghost In The Shell].mp3");
+audiotest.addEventListener('canplaythrough',()=>{
+    audiotest.play();
+})
 
 function randomColors(colors){
     // Déterminer aléatoirement différents codes composés de 4 couleurs parmi les 6 de l'array colors
@@ -32,20 +37,24 @@ function checkClicks() {
         checkCode(); // met en route la fonction checkCode
         guess =[];
         turn++;
+        if(turn === 7){
+            window.alert('You : LOSER, try again.')
+        }
     }
 }
 
 colorOptions.forEach(btn => {
-   console.log(btn)
-   const color =  btn.id; // faire correspondre un bouton à chaque couleur de mon array Color
-   console.log("color ?", btn.id)
+   // console.log(colorBtn)
+   const color =  btn.id; // faire correspondre un bouton à chaque couleur de mon array
+   // console.log("color ?", colorBtn.id)
    // A chaque couleur, faire correspondre un bouton qui permet de :
    // mettre en application la fonction pickColor quand on clique dessus
    // puis la fonction checkClicks
     btn.addEventListener('click', () => {
-        guess.push(color)
+        guess.push(color);
         pickColor(color);
         checkClicks();
+        
     });
 });
 
@@ -54,27 +63,24 @@ function pickColor(color) {
    // et la placer en fonction de round ET clicks
    // round permet de trouver la ligne de "Choose your code"
    // et clicks permet d'en trouver la colone
-   console.log(color)
-   console.log("guess > ", guessNumber);
-   console.log("clicks > ", clicks);
+   // console.log(color)
+   // console.log("guess > ", guessNumber);
+   // console.log("clicks > ", clicks);
    const guessLine = document.getElementById("guess-" + guessNumber);
    const targetColor = guessLine.querySelector(`.secretColor:nth-child(${clicks})`)
-   console.log(targetColor);
+   // console.log(targetColor);
    // targetColor.style.backgroundColor = color; 2ème option pour la ligne suivante
    targetColor.classList.add(color);   
 }
 
 function checkCode(){
-    // trouver si la couleur (secretColor) choisie par le joueur 
-    // correspond à la couleur choisie aléatoirement par la fonction randomColor
-    // ==> ce qui implique la couleur + le placement dans l'array
-    //for (let i=0; i<4; i++){
-
+    // trouver si la couleur choisie par le joueur correspond à la couleur choisie aléatoirement par la fonction randomColor
+  
     const colorsOnlyCheck = []; 
     
     guess.forEach(function(color, index) {
-        const hintLine = document.getElementById(`check-${turn}`); // définir une variable dans laquelle on sélectionne toutes les classes hint de la première ligne de vérif
-        const hintsInTheLine = hintLine.querySelector('.hint:not(.locked)'); // définir une variable dans laquelle on sélectionne la première class hint de la première ligne de vérif
+        const hintLine = document.getElementById(`check-${turn}`); // la ligne en train d'être vérifiée
+        const hintsInTheLine = hintLine.querySelector('.hint:not(.locked)'); // la première class hint de la première ligne de vérif
          // :not(.locked) pour sélectionner le hint suivant (celui qui n'est pas "bloqué") sinon on sélectionne toujours le même = le premier (car utilisation de querySelector et non pas querySelectorAll)
            
          if (code[index] === color) { // si l'emplacement ET la couleur du code défini aléatoirement et du code deviné sont identiques
@@ -83,45 +89,36 @@ function checkCode(){
         hintsInTheLine.classList.add('greenValid'); // la class hint devient vert (validé)
         hintsInTheLine.classList.add('locked'); // la class hint déjà vérifiée est "locké" afin que le loop aille sélectionner la prochaine class hint
       }
+      console.log(hintLine); 
     })
 
+    // trouver si la couleur choisie par le joueur est dans la combi secrète mais pas au bon endroit
     guess.forEach((color, index) => {
         const hintLine = document.getElementById(`check-${turn}`);
         const hintsInTheLine = hintLine.querySelector('.hint:not(.locked)');
+        console.log(hintLine);
 
         if (!colorsOnlyCheck.includes(color) && code.includes(color)){
             colorsOnlyCheck.push(color);
         
-        hintsInTheLine.classList.add('redCross'); // la class hint devient orange (à moitié validé...)
+        hintsInTheLine.classList.add('orangeCross'); // la class hint devient orange (à moitié validée...)
         hintsInTheLine.classList.add('locked'); // la class hint déjà vérifiée est "locké" afin que le loop aille sélectionner la prochaine class hint
         }
     })
-
-//function checkCode2(){
-    // déterminer si une couleur qui est dans le code deviné est dans le code défini aléatoirement 
-    //if (guess[i].includes(try2.length)); 
-    //    hintsInTheLine.classList.add('orange'); // la class hint devient vert (validé)
-    //    hintsInTheLine.classList.add('locked'); 
-
-    //const codeCopy = code.filter(word => word.length); (une façon de faire une copie)
-    //const guessCopy = guess.filter(word => word.length);
+    endGame()
+}
+    // faire apparaître une alert WIN quand une ligne de vérif affiche 4 check verts
+    // faire apparaître une alert LOSE quand aucune ligne de vérif n'affiche 4 checks verts
     
-    //let findTheMatch = guess.find(code[0]||code[1]||code[2]||code[3])
+    function endGame() {
+        const hintLine = document.getElementById(`check-${turn}`); // la ligne en train d'être vérifiée
+        const allHintsInTheLine = hintLine.querySelectorAll('.hint'); // chaque élément de chaque ligne en train d'être vérifiée
+        const newArray = [...allHintsInTheLine];
 
-    //function checkCodeTwo(colors, index){ //  si l'emplacement OU la couleur du
-    //code défini aléatoirement et du code deviné sont identiques
-    //    const codeCopy = []
-    //   const guessCopy = []
-
-    //    if (codeCopy.includes(colors, index) && guessCopy.includes(colors,index)){
-    //        hintsInTheLine.classList.add('green'); // la class hint devient ver
-    // (validé)
-    //        hintsInTheLine.classList.add('locked'); // la class hint déjà 
-    //vérifiée est "locké" afin que le loop aille sélectionner la prochaine class 
-    //hint 
+        const checkGreenValid = newArray.every(hint => {
+            return(hint.classList[1] === 'greenValid')
+        })
+        if (checkGreenValid){
+            window.alert("Congrats ! You found the hacker's code and get access back to the learning content ! (Now get in touch with Guillaume)")
         }
-
-
-//function checkCode3(){}
-    //  si l'emplacement ET la couleur du code défini aléatoirement et du code
-    // deviné sont différents
+    }
